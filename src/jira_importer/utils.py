da032474d835319679839e7978f1ceb2a480fd77
Tom_48_97 ☠ <1498046+tom4897@users.noreply.github.com>
2025-08-18 15:58:38 +0200

    Refactor utils.py to enhance type hinting and logging consistency
    - Updated function signatures in utils.py to include type hints for better clarity and type checking.
    - Replaced direct logging calls with a logger instance for improved logging practices.

--- src/jira_importer/utils.py
+++ src/jira_importer/utils.py
@@ -13,22 +13,24 @@ import logging
 import sys
 import os
 import colorlog
+from typing import Optional
 
+logger = logging.getLogger(__name__)
 
-def resource_path(relative_path):
+def resource_path(relative_path: str) -> str:
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)  # type: ignore
     return os.path.join(os.path.abspath("."), relative_path)
 # Usage: config_path = resource_path('config_importer.json')
 
-def find_config_path(config_filename, input_file_path=None, config_default=False, config_input=False):
+def find_config_path(config_filename: str, input_file_path: Optional[str] = None, config_default: bool = False, config_input: bool = False) -> str:
     search_paths = []
     
     if config_input:
         if input_file_path:
             search_paths.append(os.path.join(os.path.dirname(os.path.abspath(input_file_path)), config_filename))
         else:
-            logging.warning("config-input specified but no input file path provided")
+            logger.warning("config-input specified but no input file path provided")
             return config_filename
     elif config_default:
         search_paths.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), config_filename))
@@ -37,12 +39,12 @@ def find_config_path(config_filename, input_file_path=None, config_default=False
             search_paths.append(os.path.join(os.path.dirname(os.path.abspath(input_file_path)), config_filename))
         search_paths.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), config_filename))
     
-    logging.debug(f"Searching for configuration file in: {search_paths}")
+    logger.debug(f"Searching for configuration file in: {search_paths}")
     for path in search_paths:
         if os.path.isfile(path):
-            logging.debug(f"Found configuration file: {path}")
+            logger.debug(f"Found configuration file: {path}")
             return path
     
-    logging.warning(f"Configuration file '{config_filename}' not found in expected locations. Using default path.")
-    logging.warning(f"Expected locations: {search_paths}")
+    logger.warning(f"Configuration file '{config_filename}' not found in expected locations. Using default path.")
+    logger.warning(f"Expected locations: {search_paths}")
     return config_filename

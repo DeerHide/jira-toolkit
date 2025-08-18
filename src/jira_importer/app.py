9f1944ce6308ef70bf0fd9dd82b867cdadae9592
Tom_48_97 ☠ <1498046+tom4897@users.noreply.github.com>
2025-08-18 15:32:59 +0200

    Refactor logging in App class to use logger instance

--- src/jira_importer/app.py
+++ src/jira_importer/app.py
@@ -12,50 +12,53 @@ Date: 2025
 import logging
 import sys
 import argparse
+from artifacts import ArtifactManager
+
+logger = logging.getLogger(__name__)
 
 class App:
     # Class variable to store command line arguments
     _args = None
     
-    def __init__(self, artifact_manager):
+    def __init__(self, artifact_manager: ArtifactManager):
         self.artifact_manager = artifact_manager
 
-    def event_close(self, exit_code=0, cleanup=True):
+    def event_close(self, exit_code: int = 0, cleanup: bool = True) -> None:
         if cleanup:
             self.artifact_manager.delete_all()
-        logging.info("Jira Importer finished.")
+        logger.info("Jira Importer finished.")
         sys.exit(exit_code)
 
-    def event_abort(self, exit_code=-1):
-        logging.debug("event_abort")
+    def event_abort(self, exit_code: int = -1) -> None:
+        logger.debug("event_abort")
         
-        logging.fatal("Aborted script.")
+        logger.fatal("Aborted script.")
         self.event_close(exit_code=exit_code)
 
     @staticmethod
-    def event_fatal(exit_code=-1):
-        logging.debug("event_fatal")
+    def event_fatal(exit_code: int = -1) -> None:
+        logger.debug("event_fatal")
         
         # Show arguments if available
         if App._args:
-            logging.critical("Script failed with the following arguments:")
-            logging.critical(f"  Input file: {App._args.input_file}")
-            logging.critical(f"  Configuration: {App._args.config}")
-            logging.critical(f"  Debug mode: {App._args.debug}")
+            logger.critical("Script failed with the following arguments:")
+            logger.critical(f"  Input file: {App._args.input_file}")
+            logger.critical(f"  Configuration: {App._args.config}")
+            logger.critical(f"  Debug mode: {App._args.debug}")
             #logging.critical(f"  Import to cloud: {App._args.import_to_cloud}")
             if App._args.config_default:
-                logging.critical(f"  Config default: {App._args.config_default}")
+                logger.critical(f"  Config default: {App._args.config_default}")
             if App._args.config_input:
-                logging.critical(f"  Config input: {App._args.config_input}")
+                logger.critical(f"  Config input: {App._args.config_input}")
             if App._args.version:
-                logging.critical(f"  Version: {App._args.version}")
-            logging.critical(f" args: {App._args}")
+                logger.critical(f"  Version: {App._args.version}")
+            logger.critical(f" args: {App._args}")
         
-        logging.critical("Fatal error.")
+        logger.critical("Fatal error.")
         sys.exit(exit_code)
 
     @staticmethod
-    def parse_args():
+    def parse_args() -> argparse.Namespace:
         parser = argparse.ArgumentParser(description="This script formats a CSV file for Jira import, validating and correcting data according to specified rules.", formatter_class=argparse.RawTextHelpFormatter)
         parser.add_argument("input_file", help="Excel XLSX file", default='import.xlsx')
         

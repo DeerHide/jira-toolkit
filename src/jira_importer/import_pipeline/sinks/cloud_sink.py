50b38418b10baf51b5da42fbdb86bdc527ed343d
tom4897 <hello@tom4897.info>
2025-08-27 16:54:26 +0200

    JT-104 Added a note in the sinks for the estimate issue with Jira Cloud

--- src/jira_importer/import_pipeline/sinks/cloud_sink.py
+++ src/jira_importer/import_pipeline/sinks/cloud_sink.py
@@ -1,1 +1,5 @@
 """Cloud sink placeholder: Jira Cloud API bulk import adapter."""
+
+# NOTE, Jira CLoud: Some Jira Cloud users are affected by the historical "divide by 60" behavior. The fix (*60) will be implemented in this sink.
+#   if config.get("jira.cloud.estimate.multiply_by_60", False):
+#       row[estimate_index] = str(int(row[estimate_index]) * 60)
--- src/jira_importer/import_pipeline/sinks/csv_sink.py
+++ src/jira_importer/import_pipeline/sinks/csv_sink.py
@@ -1,1 +1,4 @@
 """CSV sink: write Jira-ready CSV output."""
+# NOTE, Jira CLoud: Some Jira Cloud users are affected by the historical "divide by 60" behavior. The fix (*60) will be implemented in this sink.
+#   if config.get("jira.cloud.estimate.multiply_by_60", False):
+#       row[estimate_index] = str(int(row[estimate_index]) * 60)
